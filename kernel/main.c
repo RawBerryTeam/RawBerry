0fb2733007e25f5492562abb9a5f0ec79fa728ee
Goldjakyt <gooldjak.official@gmail.com>
2024-10-06 15:22:41 +0200

    kernel and grub married

--- kernel/main.c
+++ kernel/main.c
@@ -5,6 +5,7 @@
  * Published on license MIT. Read `/LICENSE` for more details.
  * ---------------------------------------------------------------
 */
+#include <main.h>
 
 void launch_kernel(void);
 
---
+++ kernel/main.h
@@ -0,0 +1,15 @@
+#define MULTIBOOT_HEADER_MAGIC 0x1BADB002
+#define MULTIBOOT_HEADER_FLAGS 0x00000003
+#define MULTIBOOT_CHECKSUM -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
+
+typedef struct {
+    uint32_t magic;
+    uint32_t flags;
+    uint32_t checksum;
+} multiboot_header_t __attribute__((packed));
+
+multiboot_header_t multiboot_header __attribute__((section(".multiboot"))) = {
+    .magic = MULTIBOOT_HEADER_MAGIC,
+    .flags = MULTIBOOT_HEADER_FLAGS,
+    .checksum = MULTIBOOT_CHECKSUM,
+};
