64322c3d41a099f486387c1d1ccaed1eedcf55d1
Gorciu <gorciuyt@gmail.com>
2024-10-06 15:24:37 +0200

    declarations

---
+++ include/declarations.h
@@ -0,0 +1,11 @@
+/* 
+ * ---------------------------------------------------------------
+ *                            RawBerry
+ * (c) RawBerry Team
+ * Published on license MIT. Read `/LICENSE` for more details.
+ * ---------------------------------------------------------------
+*/
+
+void main(void);
+void launch_kernel(void);
+void kernel_panic(void);
