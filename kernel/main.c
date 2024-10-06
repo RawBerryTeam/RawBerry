48518c28b47345e71a2fe15b46ddb345ef21dc82
Goldjakyt <gooldjak.official@gmail.com>
2024-10-06 15:34:56 +0200

    ok vga print add

--- kernel/main.c
+++ kernel/main.c
@@ -6,10 +6,13 @@
  * ---------------------------------------------------------------
 */
 #include "main.h"
+#include "../drivers/vga.h"
 
 /**
  * Launches kernel.
  */
 void launch_kernel(void) {
-
+    vga_initialize();
+    vga_print_string("Hello, RawBerry OS!\n");
+    vga_print_string("Welcome to your custom kernel.");
 }
