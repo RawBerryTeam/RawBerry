e1bf1c40ea2719727f36c40ea1b2b52b989845fa
Gorciu <gorciuyt@gmail.com>
2024-10-06 11:40:20 +0200

    nothing

--- init/start.c
+++ init/start.c
@@ -5,3 +5,10 @@
  * Published on license MIT. Read `/LICENSE` for more details.
  * ---------------------------------------------------------------
 */
+
+/**
+ * Main function. Here kernel will start its work.
+ */
+int main() {
+    return 0;
+}
