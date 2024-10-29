6b9385f618e8d45306da6bba2941aa2e281c1ee5
Reggie <ben@bendaws.net>
2024-10-29 13:46:14 -0500

    Safe booting work

--- kernel/main.c
+++ kernel/main.c
@@ -5,16 +5,27 @@
  * Published on license MIT. Read `/LICENSE` for more details.
  * ---------------------------------------------------------------
 */
+
+// these checks just ensure you use a cross-compiler
+// in case you dont, your system probably wont work
+#if defined(__linux__)
+#error "Use a cross-compiler for ix86"
+#endif
+
+// 32bit targets
+#if !defined(__i386__)
+#error "Use a cross-compiler for elf32-i386"
+#endif
+
 #include "main.h"
 #include "../drivers/vga.h"
 #include "../drivers/port.h"
-#include "systemio.h"
 #include "../include/stdio.h" // :: There is no standard yet, either switch to "stdio.h" or comment it out until
 //                    :: there is implementation for it
 
 void launch_kernel(void) {
     vga_initialize();
-    out("kernel", SYSTEMIO_OUT_INFO, "Kernel has initialized");
+    printf("kernel init")
     print_log(); //hehehe
     printf("Welcome to RawBerry!\n");
 }
